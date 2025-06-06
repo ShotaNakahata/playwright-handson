c67f6f647e99cda44ec5e02d5a0259a362674891
中畠昇汰 <pandora.osk@gmail.com>
2025-06-06 12:13:44 +0800

    Add test: Form API mocking test completed

--- tests/form.spec.ts
+++ tests/form.spec.ts
@@ -18,3 +18,18 @@ test("フォーム操作のテスト", async ({ page }) => {
 		/(項羽→劉邦)|(劉邦→項羽)/,
 	);
 });
+
+test("フォーム操作のテスト（サーバーモック）", async ({ page }) => {
+	await page.route("/api/shuffle", async (route) => {
+		const json = { members: ["張飛", "関羽", "劉備"] };
+		await route.fulfill({ json });
+	});
+	await page.goto("http://localhost:3000/form");
+	await page.getByRole("textbox", { name: /1 人目/ }).fill("劉備");
+	await page.getByRole("textbox", { name: /2 人目/ }).fill("関羽");
+	await page.getByRole("textbox", { name: /3 人目/ }).fill("張飛");
+	await page.getByRole("button", { name: /シャッフル/ }).click();
+	await expect(page.getByRole("status", { name: /結果/ })).toHaveText(
+		/張飛→関羽→劉備/,
+	);
+});
